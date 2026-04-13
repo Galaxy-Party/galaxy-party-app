9f9f9d802e72030999a54c2638475ea5616f0c80
Adrien Degand <adrien.degand@hotmail.com>
2026-04-13 14:31:55 +0200

    [FEAT] 16: Liaison room/user

--- galaxy-party-backend/src/main/java/com/galaxy_party/backend/dto/room/output/RoomDto.java
+++ galaxy-party-backend/src/main/java/com/galaxy_party/backend/dto/room/output/RoomDto.java
@@ -1,10 +1,12 @@
 package com.galaxy_party.backend.dto.room.output;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.util.UUID;
 
 @Builder
+@Data
 public class RoomDto {
     private UUID id;
 }
--- galaxy-party-backend/src/main/java/com/galaxy_party/backend/services/RoomService.java
+++ galaxy-party-backend/src/main/java/com/galaxy_party/backend/services/RoomService.java
@@ -2,7 +2,9 @@ package com.galaxy_party.backend.services;
 
 import com.galaxy_party.backend.dto.room.input.CreateRoomDto;
 import com.galaxy_party.backend.entity.RoomEntity;
+import com.galaxy_party.backend.entity.UserEntity;
 import com.galaxy_party.backend.repository.RoomRepository;
+import com.galaxy_party.backend.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import java.util.UUID;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+    private final UserRepository userRepository;
 
     public List<RoomEntity> findAll() {
         return roomRepository.findAll();
@@ -30,8 +33,45 @@ public class RoomService {
     }
 
     public void deleteRoom(UUID id) {
+
+        RoomEntity room = roomRepository.findById(id)
+                .orElseThrow();
+
+        for (UserEntity user : room.getUsers()) {
+            user.setRoom(null);
+        }
+
+
         roomRepository.deleteById(id);
     }
 
 
+
+    public Boolean joinRoom(UUID roomId, UUID userId) {
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        RoomEntity room = this.findById(roomId);
+
+        List<UserEntity> users = room.getUsers();
+
+        if (users == null){
+            room.setUsers(List.of(userEntity));
+        } else {
+            if (users.contains(userEntity)){
+                return false;
+            }
+
+            users.add(userEntity);
+
+            room.setUsers(users);
+        }
+
+        userEntity.setRoom(room);
+
+        userRepository.save(userEntity);
+        roomRepository.save(room);
+
+        return true;
+    }
 }
